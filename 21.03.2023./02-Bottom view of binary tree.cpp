012f355c8941eb2b642945d840ea2bf031b9b202
Manish <75516081+Manish263@users.noreply.github.com>
2023-03-22 22:30:07 +0530

    Create 02-Bottom view of binary tree.cpp

---
+++ 21.03.2023./02-Bottom view of binary tree.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    vector <int> bottomView(Node *root) {
+        // Your Code Here
+        vector<int>res;
+        if(!root) return res;
+//in map we inserted level and node as key value to get top view.
+        map<int,int>m;
+        queue<pair<Node*,int>>q;
+//in queue we have inserted node with line.
+        q.push({root,0});
+        while(!q.empty()){
+            auto it = q.front(); q.pop();
+            Node* node = it.first;
+            int line = it.second;
+            m[line]=node->data;
+            if(node->left!=NULL) q.push({node->left,line-1});
+            if(node->right!=NULL) q.push({node->right,line+1});
+        }
+//we pushed map elements into vec.
+        for(auto t: m){
+            res.push_back(t.second);
+        }
+        return res;
+    }
+};
